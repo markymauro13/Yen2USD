acb6e159b0d0e8a185ff22d736ac4d0864e40ec5
Kenneth Doerflein <ktd500@verizon.net>
2024-04-25 01:18:52 -0400

    add gitignore file

--- scripts/SET_API_KEY_TEMPLATE.js
+++ scripts/SET_API_KEY_TEMPLATE.js
@@ -1,6 +1,5 @@
 // DO NOT PUT YOUR API KEY IN THIS TEMPLATE FILE
-// ADD `/scripts/SET_API_KEY.js`.gitignore
-// THEN USE THIS AS A TEMPLATE TO CREATE `SET_API_KEY.js in the `scripts` directory
+// USE THIS AS A TEMPLATE TO CREATE `SET_API_KEY.js in the `scripts` directory
 
 // DO NOT PUT YOUR API KEY IN THIS TEMPLATE FILE
 // This is where you will put your API key in `/scripts/SET_API_KEY.js`
